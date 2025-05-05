81c8b992336a6121183a3f08dd2ed945d5841abf
leticiad3br27 <leticiaSilva271107@gmail.com>
2025-05-05 10:01:36 -0400

    editei essa bomba

--- amavi-api/db/db.js
+++ amavi-api/db/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise'); // Certifique-se de usar mysql2/promise
 
 const connection = mysql.createPool({ // Usar createPool para melhor gerenciamento de conexões
-  host: '200.129.130.149',
+  host: 'icskko8k08w0ss88kws80s0o',
   user: 'amavi',
   password: '12345678',
   database: 'amavi_bd',
-  port: 20002
+  port:3306
 });
 
 module.exports = connection;

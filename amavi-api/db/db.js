ab1b92a4b6cd93c4ffe7ef16f98089b8dc32acce
leticiad3br27 <leticiaSilva271107@gmail.com>
2025-05-16 20:41:55 -0400

    editei

--- amavi-api/db/db.js
+++ amavi-api/db/db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2/promise'); 
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({ 
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+const connection = mysql.createPool({
+  host: 'icskko8k08w0ss88kws80s0o', // Seu host ou IP do banco
+  user: 'seu_usuario',              // Coloque seu usuário do MySQL aqui
+  password: 'sua_senha',            // Sua senha do MySQL
+  database: 'seu_banco',            // Nome do seu banco de dados
+  port: 3306                       // Porta padrão do MySQL
 });
 
 module.exports = connection;

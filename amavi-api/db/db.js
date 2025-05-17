875ca310fa26d9cb4478b12290980b8409b2e02e
leticiad3br27 <leticiaSilva271107@gmail.com>
2025-05-16 20:43:58 -0400

    agora vai

--- amavi-api/db/db.js
+++ amavi-api/db/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'icskko8k08w0ss88kws80s0o', // Seu host ou IP do banco
-  user: 'seu_usuario',              // Coloque seu usuário do MySQL aqui
-  password: 'sua_senha',            // Sua senha do MySQL
-  database: 'seu_banco',            // Nome do seu banco de dados
+  host: 'icskko8k08w0ss88kws80s0o', // Host do banco
+  user: 'amavi',                   // Usuário do MySQL
+  password: '12345678',            // Senha do MySQL
+  database: 'amavi_bd',            // Nome do banco de dados
   port: 3306                       // Porta padrão do MySQL
 });
 

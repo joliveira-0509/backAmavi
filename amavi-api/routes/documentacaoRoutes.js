6738b7a8051d4b73b24404ace5c2ed22b4c9184d
leticiad3br27 <leticiaSilva271107@gmail.com>
2025-07-13 18:15:22 -0400

    arrumaaaa

--- amavi-api/routes/documentacaoRoutes.js
+++ amavi-api/routes/documentacaoRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const multer = require('multer');
 const DocumentacaoController = require('../controllers/documentacaoController');
-const { autenticarToken } = require('../controllers/loginController');
 
 // Multer - armazenamento em memória para pegar buffer
 const storage = multer.memoryStorage();
@@ -20,12 +19,12 @@ const upload = multer({
 
 const router = express.Router();
 
-router.post('/documentos', autenticarToken, upload.single('documento'), DocumentacaoController.cadastrar);
-router.get('/documentos', autenticarToken, DocumentacaoController.listarTodas);
-router.get('/documentos/:id', autenticarToken, DocumentacaoController.buscarPorId);
-router.get('/documentos/arquivo/:id', autenticarToken, DocumentacaoController.buscarArquivo);
-router.get('/documentos/usuario/:id_usuario', autenticarToken, DocumentacaoController.buscarPorUsuario);
-router.put('/documentos/:id', autenticarToken, upload.single('documento'), DocumentacaoController.editar);
-router.delete('/documentos/:id', autenticarToken, DocumentacaoController.deletar);
+router.post('/documentos', upload.single('documento'), DocumentacaoController.cadastrar);
+router.get('/documentos', DocumentacaoController.listarTodas);
+router.get('/documentos/:id', DocumentacaoController.buscarPorId);
+router.get('/documentos/arquivo/:id', DocumentacaoController.buscarArquivo);
+router.get('/documentos/usuario/:id_usuario', DocumentacaoController.buscarPorUsuario);
+router.put('/documentos/:id', upload.single('documento'), DocumentacaoController.editar);
+router.delete('/documentos/:id', DocumentacaoController.deletar);
 
 module.exports = router;

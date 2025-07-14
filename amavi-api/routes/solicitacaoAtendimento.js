c2d556574fff6269596af34bdc254867d3213030
leticiad3br27 <leticiaSilva271107@gmail.com>
2025-07-13 22:22:18 -0400

    arrumadno o bhack

--- amavi-api/routes/solicitacaoAtendimento.js
+++ amavi-api/routes/solicitacaoAtendimento.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const SolicitacaoAtendimentoController = require('../controllers/solicitacaoAtendimentoController');
-const { autenticarToken } = require('../controllers/loginController'); // <-- Adicione aqui
 
 // Criar uma nova solicitação (requerimento)
-router.post('/solicitacao', autenticarToken, SolicitacaoAtendimentoController.cadastrar);
+router.post('/solicitacao', SolicitacaoAtendimentoController.cadastrar);
 
 // Listar todas as solicitações
-router.get('/solicitacao', autenticarToken, SolicitacaoAtendimentoController.listarTodas);
+router.get('/solicitacao', SolicitacaoAtendimentoController.listarTodas);
 
 // Buscar solicitação por ID
-router.get('/solicitacao/:id', autenticarToken, SolicitacaoAtendimentoController.buscarPorId);
+router.get('/solicitacao/:id', SolicitacaoAtendimentoController.buscarPorId);
 
 // Buscar solicitações por usuário
-router.get('/solicitacao/usuario/:id_usuario', autenticarToken, SolicitacaoAtendimentoController.buscarPorUsuario);
+router.get('/solicitacao/usuario/:id_usuario', SolicitacaoAtendimentoController.buscarPorUsuario);
 
 // Editar uma solicitação
-router.put('/solicitacao/:id', autenticarToken, SolicitacaoAtendimentoController.editar);
+router.put('/solicitacao/:id', SolicitacaoAtendimentoController.editar);
 
 // Deletar uma solicitação
-router.delete('/solicitacao/:id', autenticarToken, SolicitacaoAtendimentoController.deletar);
+router.delete('/solicitacao/:id', SolicitacaoAtendimentoController.deletar);
 
 module.exports = router;
